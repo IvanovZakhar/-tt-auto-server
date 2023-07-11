22b7b237ff2246b81256e9f98bea74491fbfa34b
Zakhar <uvanov977777@gmail.com>
2023-07-11 11:24:26 +0300

    update key

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 
 // Строка подключения к MongoDB
-const url = process.env.MONGO_URL ;  
+const url = 'mongodb://hrTest:hTy785JbnQ5@mongo0.maximum.expert:27423/?authSource=hrTest&replicaSet=ReplicaSet&readPreference=primary';
+
 // Имя базы данных и коллекции
 const dbName = 'hrTest';
 const collectionName = 'stock';
